2e5dbbc27a77d4222dd62bcc6102f057c2feba2b
Nathan Víctor de Faria <nathan.faria@inatel.br>
2022-06-18 16:51:45 -0300

    Function added included and working

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, {Fragment, useState} from "react";
 
 const InputTodo = () => {
-    return <h1>Input Todo</h1>
+    const [description, setDescription] = useState("Type here");
+
+    const onSubmitForm = async e => {
+        e.preventDefault();
+        try {
+            const body = {description};
+            const response = await fetch("http://localhost:5000/todos", {
+                method: "POST",
+                headers: {"Content-type": "application/json"},
+                body: JSON.stringify(body)
+            });
+            console.log(response);
+        } catch (e) {
+            console.error(e.message);
+        }
+    }
+
+    return (
+        <Fragment>
+            <h1 className="text-center mt-5">Pern Todo list</h1>
+            <form className="d-flex mt-5" onSubmit={onSubmitForm}> 
+                <input type="text" className="form-control" value={description} onChange={e => setDescription(e.target.value)} />
+                <button className="btn btn-success">Add</button>
+            </form>
+        </Fragment>
+    )
 };
 
 export default InputTodo;
