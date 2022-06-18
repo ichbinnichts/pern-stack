fba02ca67a4faeec2f2bb8b5a5c2bc1324b05863
Nathan Víctor de Faria <nathan.faria@inatel.br>
2022-06-18 17:01:26 -0300

    Starting the development of the list todos

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 //Components
 import InputTodo from "./components/InputTodo";
+import ListTodo from "./components/ListTodos";
 
 function App() {
   return (
     <Fragment>
       <div className="container">
         <InputTodo />
+        <ListTodo />
       </div>
     </Fragment>
   );
---
+++ client/src/components/ListTodos.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const ListTodo = () => {
+    return <h1>List Todos</h1>;};
+
+export default ListTodo;
